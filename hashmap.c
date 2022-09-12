232bc2329275b22b243647367766e85dfe935b33
Replit user <>
2022-09-12 01:30:57 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -118,8 +118,10 @@ Pair * nextMap(HashMap * map) {
   Pair * aux = (Pair *)malloc(sizeof(Pair));
   int i;
   for(i=map->current+1;i < map->capacity;i++){
-    if(i==map->capacity)
-      return NULL;
+    if(i==map->capacity){
+      aux=NULL;
+      break;
+    }
     if(map->buckets[i] != NULL && map->buckets[i]->key != NULL){
       aux=map->buckets[i];
       break;
